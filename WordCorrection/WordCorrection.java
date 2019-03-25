4bb486601848d2fc152d55c69d810c2288691212
hill2hill <all4git@gmail.com>
2019-03-25 11:21:18 +0800

    v1.1-fix recommend bug

--- WordCorrection/WordCorrection.java
+++ WordCorrection/WordCorrection.java
@@ -138,22 +138,22 @@ class Dictionary {
             int similarity = 0;
             for(int i = 0;i<26;i++){
                 similarity += ((int[])dict_vector.get(index))[i]* wrong_word_vector[i];
-                if(similarity>rank_value[0]){
-                    rank_pos[2] = rank_pos[1];
-                    rank_value[2] = rank_value[1];
-                    rank_pos[1] = rank_pos[0];
-                    rank_value[1] = rank_value[0];
-                    rank_pos[0] = index;
-                    rank_value[0] = similarity;
-                }else if(similarity>rank_value[1]){
-                    rank_pos[2] = rank_pos[1];
-                    rank_value[2] = rank_value[1];
-                    rank_pos[1] = index;
-                    rank_value[1] = similarity;
-                }else if(similarity>rank_value[2]){
-                    rank_pos[2] = index;
-                    rank_value[2] = similarity;
-                }
+            }
+            if(similarity>rank_value[0]){
+                rank_pos[2] = rank_pos[1];
+                rank_value[2] = rank_value[1];
+                rank_pos[1] = rank_pos[0];
+                rank_value[1] = rank_value[0];
+                rank_pos[0] = index;
+                rank_value[0] = similarity;
+            }else if(similarity>rank_value[1]){
+                rank_pos[2] = rank_pos[1];
+                rank_value[2] = rank_value[1];
+                rank_pos[1] = index;
+                rank_value[1] = similarity;
+            }else if(similarity>rank_value[2]){
+                rank_pos[2] = index;
+                rank_value[2] = similarity;
             }
         }
         for(int i = 0;i<3;i++){
